80cfd610d28074cb8e31e61bd2e7c83201718d75
mfernanda29 <117603869+mfernanda29@users.noreply.github.com>
2024-06-11 10:18:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from sklearn.base import TransformerMixin
 from sklearn.preprocessing import RobustScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 
-df_ACC_TRA = pd.read_csv('Data\Accidentes_de_transito_en_carreteras-2020-2021-Sutran.csv', encoding='utf8', delimiter=';')
+df_ACC_TRA = pd.read_csv('Data/Accidentes_de_transito_en_carreteras-2020-2021-Sutran.csv', encoding='utf8', delimiter=';')
 
 columnaCodigoVia = []
 
